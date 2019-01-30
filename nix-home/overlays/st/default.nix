6d1bc613bc2b79221e23c57919db66ca00f61080
vasile <elsile69@yahoo.com>
2019-01-30 23:55:36 +0200

    st terminal code fix

--- nix-home/overlays/st/default.nix
+++ nix-home/overlays/st/default.nix
@@ -1,9 +1,11 @@
 self: super:
 {
   st = with super; st.override (old: rec {
-    patches = [ ./st-scrollback-mouse-0.8.diff
-    ./st-scrollback-0.8.diff
-    ./st-xresources-20180309-c5ba9c0.diff
-    ./st-vertcenter-20180320-6ac8c8a.diff ];
+    patches = [
+      ./st-scrollback-mouse-0.8.diff
+      ./st-scrollback-0.8.diff
+      ./st-xresources-20180309-c5ba9c0.diff
+      ./st-vertcenter-20180320-6ac8c8a.diff
+    ];
   });
 }
