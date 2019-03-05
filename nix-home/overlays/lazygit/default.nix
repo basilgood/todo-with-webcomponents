3dbd9dffb1a70f9ce4f6ce85101a07e07c280aad
vasile <elsile69@yahoo.com>
2019-03-05 22:02:53 +0200

    lazygit 0.7.2 upgrade

--- nix-home/overlays/lazygit/default.nix
+++ nix-home/overlays/lazygit/default.nix
@@ -3,12 +3,12 @@ with super;
 {
   lazygit = lazygit.overrideAttrs (old: rec {
     name = "lazygit-${version}";
-    version = "0.6";
+    version = "0.7.2";
     src = fetchFromGitHub {
       owner = "jesseduffield";
       repo = "lazygit";
       rev = "v${version}";
-      sha256 = "1a6kj4agcbyji0dbn72ph9zfq074vd3dsxyx43x1qjkil35r62i5";
+      sha256 = "1b5mzmxw715cx7b0n22hvrpk0dbavzypljc7skwmh8k1nlx935jj";
     };
   });
 }
