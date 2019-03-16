7455df32238eb16f07cf180b2fea292406c9388c
vasile <elsile69@yahoo.com>
2019-03-17 00:18:22 +0200

    st terminal Xresouces config and some patches

--- nix-home/overlays/st/default.nix
+++ nix-home/overlays/st/default.nix
@@ -3,8 +3,8 @@ self: super:
   st = with super; st.override (old: rec {
     patches = [
       ./st-scrollback-mouse-0.8.diff
-      ./st-scrollback-0.8.diff
-      ./st-xresources-20180309-c5ba9c0.diff
+      ./st-scrollback-20190122-3be4cf1.diff
+      ./st-xresources-20190105-3be4cf1.diff
       ./st-vertcenter-20180320-6ac8c8a.diff
     ];
   });
