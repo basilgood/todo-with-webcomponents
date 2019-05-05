1a09a4ae50632d675f2161422e3839fdb9603007
vasile <elsile69@yahoo.com>
2019-05-05 16:44:52 +0300

    [cleanup] remove unused window managers

--- nix-home/config.nix
+++
@@ -1,9 +0,0 @@
-{pkgs}:
-{
-  allowUnfree = true;
-  # packageOverrides = pkgs: {
-  #   chromium = pkgs.chromium.override {
-  #     commandLineArgs = "--enable-native-gpu-memory-buffers --enable-gpu-rasterization --ignore-gpu-blacklist --enable-zero-copy --force-gpu-rasterization";
-  #   };
-  # };
-}
--- nix-home/overlays/dmenu/default.nix
+++
@@ -1,8 +0,0 @@
-self: super:
-{
-  dmenu = with super; dmenu.override (old: rec {
-    patches = [
-      ./dmenu-fuzzymatch-4.6.diff
-    ];
-  });
-}
