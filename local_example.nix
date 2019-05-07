5dc6c766ce8af9c213a5dd791bc9e0974542d95b
vasile <elsile69@yahoo.com>
2019-05-07 03:50:58 +0300

    [local_example] updates

--- local_example.nix
+++ local_example.nix
@@ -1,18 +1,18 @@
 { config, lib, pkgs, ... }:
 {
-  networking.hostName = "__hostname__";
-  time.timeZone = "Europe/Bucharest";
-
+  networking.hostName = "_host_";
+  time.timeZone = "__/__";
 
   users = {
-    groups.__user__ = {
+    groups._user_ = {
       gid = 1000;
     };
-    users.__user__ = {
+    users._user_ = {
       isNormalUser = true;
       uid = 1000;
-      extraGroups = ["__user__" "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "lxd" "docker"];
-      initialPassword = "__user__";
+      extraGroups = ["_user_" "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "lxd" "docker" "vboxusers" "sway"];
+      initialPassword = "_user_";
     };
   };
- }
+  system.stateVersion = "19.09"; # Did you read the comment?
+}
