1c66b071c2096ac1d969584f29fdd1652a73d0df
vasile <elsile69@yahoo.com>
2019-05-11 13:39:59 +0300

    [tig] update to latest commit and new config

--- overlays/tig/default.nix
+++ overlays/tig/default.nix
@@ -6,7 +6,21 @@ with super;
 
       name = "tig-with-config";
 
-      paths = [gitAndTools.tig];
+      paths = [
+        (gitAndTools.tig.overrideAttrs(old: rec {
+          pname = "tig";
+          version = "2.4.2";
+          name = "${pname}-${version}";
+
+          src = fetchFromGitHub {
+            owner = "jonas";
+            repo = pname;
+            rev = "ea43f8be22c7b985c7c752d938579fe14d8e9f08";
+            sha256 = "1x95rvg8m85cda5nl0nwlmkqkar0ahf4sw93km6mfgjhkk5igmrb";
+          };
+        }))
+      ];
+
       nativeBuildInputs = [ super.makeWrapper ];
 
       postBuild = ''
