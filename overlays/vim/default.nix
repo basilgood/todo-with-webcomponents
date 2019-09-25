5c9cc04547ee778edb94a4f23f0ec8677fbaf33b
vasile luta <elsile69@yahoo.com>
2019-09-25 22:20:15 +0300

    vim update

--- overlays/vim/default.nix
+++ overlays/vim/default.nix
@@ -2,13 +2,13 @@ self: super:
 with super; {
   vimHugeX = (vimHugeX.overrideAttrs (attrs: rec {
     name = "vim-${version}";
-    version = "8.1.2065";
+    version = "8.1.2073";
 
     src = fetchFromGitHub {
       owner = "vim";
       repo = "vim";
       rev = "v${version}";
-      sha256 = "1bgcfyy4yya1j4fzjngkqviyw4xzckkh0kn31rdy23nwx92nsjwh";
+      sha256 = "1hdw2mqygm6bq3zll1hx5d82qx297fk6kywkcfmy5xhwlxns1mwm";
     };
   })).override { python = python3; };
 }
