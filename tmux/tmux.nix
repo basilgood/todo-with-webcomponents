e0ceb182eff6c2a1d32657ba42eb1c05727a0237
vasile luta <elsile69@yahoo.com>
2019-11-11 05:35:15 +0200

    tmux: nord theme

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -4,56 +4,49 @@
   terminal = "tmux-256color";
   keyMode = "vi";
   extraTmuxConf = ''
-    # Automatically set window title
-    set-window-option -g automatic-rename on
-    set-option -g set-titles on
-    set-option -g renumber-windows on
-    set-window-option -g xterm-keys on
-    set -ga terminal-overrides ",xterm-256color:Tc"
-    set -g focus-events on
-    set-option -g -q mouse on
-    setw -g monitor-activity on
-    bind -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M; send-keys -M; send-keys -M; send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M; send-keys -M; send-keys -M; send-keys -M' 'copy-mode -e; send-keys -M; send-keys -M; send-keys -M; send-keys -M'"
-    bind -n WheelDownPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M; send-keys -M; send-keys -M; send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M; send-keys -M; send-keys -M; send-keys -M' 'copy-mode -e; send-keys -M; send-keys -M; send-keys -M; send-keys -M'"
-
-    unbind C-b
-    set -g prefix C-Space
-    bind C-Space send-prefix
-    bind -n M-v split-window -h
-    bind -n M-s split-window -v
-    bind -n M-j resize-pane -D 5
-    bind -n M-k resize-pane -U 5
-    bind -n M-h resize-pane -L 5
-    bind -n M-l resize-pane -R 5
-    bind -n M-Left select-pane -L
-    bind -n M-Right select-pane -R
-    bind -n M-Up select-pane -U
-    bind -n M-Down select-pane -D
-    bind -n M-,  previous-window
-    bind -n M-. next-window
-
-    # Default colors
-    set-option -g status-style fg=colour145,bg=colour235
-
-    # Window titles
-    set-window-option -g window-status-style fg=default,bg=default
-    set-window-option -g window-status-current-style fg=white,bold,bg=default
-    set-window-option -g window-status-activity-style fg=default,noreverse,bg=default
-
-    # Pane borders
-    set-option -g pane-border-style fg=colour240,bg=default
-    set-option -g pane-active-border-style fg=colour76,bg=default
-
-    # Message text
-    set-option -g message-style fg=colour76,bg=default
-    set-option -g message-command-style fg=colour75,bg=default
-
-    # Copy mode
-    set-window-option -g mode-style fg=default,bg=colour240
-    set -g status-left-length 30
-    set -g  status-left '#S:  '
-    set -g window-status-separator '  '
-    set -g status-right '#{?client_prefix, prefix ,}'
+      # Automatically set window title
+      set-window-option -g automatic-rename on
+      set-option -g set-titles on
+      set-option -g renumber-windows on
+      set-window-option -g xterm-keys on
+      set -ga terminal-overrides ",xterm-256color:Tc"
+      set -g focus-events on
+      setw -g mouse on
+      setw -g monitor-activity on
+      unbind C-b
+      set -g prefix C-Space
+      bind C-Space send-prefix
+      # Default colors
+      set-option -g status-style fg=white,bg=black
+      # Status
+      set -g status-interval 1
+      set-option -g status-left-length 40
+      # Window titles
+      set-window-option -g window-status-style fg=default,bg=default
+      set-window-option -g window-status-current-style fg=white,bold,bg=default
+      set-window-option -g window-status-activity-style fg=default,noreverse,bg=default
+      # Pane borders
+      set-option -g pane-border-style fg=colour240,bg=default
+      set-option -g pane-active-border-style fg=colour76,bg=default
+      # Message text
+      set-option -g message-style fg=colour76,bg=default
+      set-option -g message-command-style fg=colour75,bg=default
+      # Bars
+      set -g status-left "#[fg=black,bg=blue,bold] #S #[fg=blue,bg=black,nobold,noitalics,nounderscore]"
+      set -g status-right "#{prefix_highlight}#[fg=brightblack,bg=black,nobold,noitalics,nounderscore]#[fg=cyan,bg=brightblack] #[fg=black,bg=cyan,bold] #H "
+      # Windows
+      set -g window-status-format "#[fg=black,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#I #[fg=white,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#W #F #[fg=brightblack,bg=black,nobold,noitalics,nounderscore]"
+      set -g window-status-current-format "#[fg=black,bg=cyan,nobold,noitalics,nounderscore] #[fg=black,bg=cyan]#I #[fg=black,bg=cyan,nobold,noitalics,nounderscore] #[fg=black,bg=cyan]#W #F #[fg=cyan,bg=black,nobold,noitalics,nounderscore]"
+      set -g window-status-separator ""
+      # Panes
+      set -g pane-border-style "bg=black fg=black"
+      set -g pane-active-border-style "bg=black fg=brightblack"
+      # set -g display-panes-colour black
+      # set -g display-panes-active-colour brightblack
+      # tmux-prefix-highlight
+      set -g @prefix_highlight_output_prefix "#[fg=brightcyan]#[bg=black]#[nobold]#[noitalics]#[nounderscore]#[bg=brightcyan]#[fg=black]"
+      set -g @prefix_highlight_output_suffix ""
+      set -g @prefix_highlight_copy_mode_attr "fg=brightcyan,bg=black,bold"
     run-shell ${pkgs.tmuxPlugins.pain-control.rtp}
     run-shell ${pkgs.tmuxPlugins.sensible.rtp}
     run-shell ${pkgs.tmuxPlugins.resurrect.rtp}
