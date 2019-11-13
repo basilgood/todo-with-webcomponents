6a4ed1986cdc87a58cd3ff9b48e23dfdf433c367
vasile luta <elsile69@yahoo.com>
2019-11-13 10:08:09 +0200

    tmux: fix

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -16,39 +16,42 @@
       unbind C-b
       set -g prefix C-Space
       bind C-Space send-prefix
+
       # Default colors
       set-option -g status-style fg=white,bg=black
+
       # Status
       set -g status-interval 1
-      set-option -g status-left-length 40
+      set -g status-justify left
+      set -g status-left-length 40
+
       # Window titles
       set-window-option -g window-status-style fg=default,bg=default
       set-window-option -g window-status-current-style fg=white,bold,bg=default
       set-window-option -g window-status-activity-style fg=default,noreverse,bg=default
-      # Pane borders
-      set-option -g pane-border-style fg=colour240,bg=default
-      set-option -g pane-active-border-style fg=colour76,bg=default
-      # Message text
-      set-option -g message-style fg=colour76,bg=default
-      set-option -g message-command-style fg=colour75,bg=default
+
       # Bars
+      set -g window-status-separator ""
       set -g status-left "#[fg=black,bg=blue,bold] #S #[fg=blue,bg=black,nobold,noitalics,nounderscore]"
       set -g status-right "#{prefix_highlight}#[fg=brightblack,bg=black,nobold,noitalics,nounderscore]#[fg=cyan,bg=brightblack] #[fg=black,bg=cyan,bold] #H "
+
       # Windows
       set -g window-status-format "#[fg=black,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#I #[fg=white,bg=brightblack,nobold,noitalics,nounderscore] #[fg=white,bg=brightblack]#W #F #[fg=brightblack,bg=black,nobold,noitalics,nounderscore]"
       set -g window-status-current-format "#[fg=black,bg=cyan,nobold,noitalics,nounderscore] #[fg=black,bg=cyan]#I #[fg=black,bg=cyan,nobold,noitalics,nounderscore] #[fg=black,bg=cyan]#W #F #[fg=cyan,bg=black,nobold,noitalics,nounderscore]"
       set -g window-status-separator ""
+
       # Panes
-      set -g pane-border-style "bg=black fg=black"
-      set -g pane-active-border-style "bg=black fg=brightblack"
-      # set -g display-panes-colour black
-      # set -g display-panes-active-colour brightblack
+      set -g pane-border-style bg=default,fg=black
+      set -g pane-active-border-style bg=default,fg=blue
+
       # tmux-prefix-highlight
       set -g @prefix_highlight_output_prefix "#[fg=brightcyan]#[bg=black]#[nobold]#[noitalics]#[nounderscore]#[bg=brightcyan]#[fg=black]"
       set -g @prefix_highlight_output_suffix ""
       set -g @prefix_highlight_copy_mode_attr "fg=brightcyan,bg=black,bold"
-    run-shell ${pkgs.tmuxPlugins.pain-control.rtp}
-    run-shell ${pkgs.tmuxPlugins.sensible.rtp}
-    run-shell ${pkgs.tmuxPlugins.resurrect.rtp}
+
+      run-shell ${pkgs.tmuxPlugins.pain-control.rtp}
+      run-shell ${pkgs.tmuxPlugins.sensible.rtp}
+      run-shell ${pkgs.tmuxPlugins.resurrect.rtp}
+      run-shell ${pkgs.tmuxPlugins.prefix-highlight.rtp}
   '';
 }
