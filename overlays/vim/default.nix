b92f6067abb2cf59d8a6483cce3882f9a30be300
vasile luta <elsile69@yahoo.com>
2019-12-20 09:57:25 +0200

    vim: declarative

--- overlays/vim/default.nix
+++ overlays/vim/default.nix
@@ -1,6 +1,13 @@
 self: super:
-with super; {
-  vimHugeX = (vimHugeX.overrideAttrs (attrs: rec {
+with super;
+let
+  vimrc = callPackage ./vimrc.nix { };
+  options = callPackage ./options.nix { };
+  mappings = callPackage ./mappings.nix { };
+  # customPlugins = import ./plugins.nix { inherit self super; };
+  # allPlugins = vimPlugins // customPlugins;
+in {
+  vim = ((vim_configurable.overrideAttrs (attrs: rec {
     name = "vim-${version}";
     version = "8.2.0008";
 
@@ -10,5 +17,20 @@ with super; {
       rev = "v${version}";
       sha256 = "09vxwjsywcgg426sshxbxwzm08v8qniqzask3zgyc37fwwmgsnqh";
     };
-  })).override { python = python3; ftNixSupport = false;};
-}
+  })).override {
+    python = python3;
+    ftNixSupport = false;
+  }).customize {
+    name = "vim";
+    vimrcConfig = {
+      packages.myVimPackage = with vimPlugins; {
+        start = [ vim-nix  ];
+      };
+      customRC = ''
+          ${vimrc}
+          ${options}
+          ${mappings}
+        '';
+      };
+    };
+    }
