3bf6dde789f4c6ce4d10ac681920696d5b81e425
vasile luta <elsile69@yahoo.com>
2020-01-06 08:42:30 +0200

    lazygit: latest commit

--- overlays/lazygit/default.nix
+++ overlays/lazygit/default.nix
@@ -6,8 +6,8 @@ with super;
     src = fetchFromGitHub {
       owner = "jesseduffield";
       repo = "lazygit";
-      rev = "710abded6416c51f72285854c332881efab3e810";
-      sha256 = "06d7dz79gbd42ymqv48xlpxnhrri8mp4s0721ia410n0jxc56rsg";
+      rev = "96ca7262e4b66e4de7cb8b4859cc2b3117bcffe8";
+      sha256 = "1qb0mgbl8nxkwk6zxylm25d9j4pzp0a6zi140b106igid5c7h5m2";
     };
   });
 }
