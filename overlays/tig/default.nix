77298a5da07ada3b93cd0c7d6457b8356289095d
vasile luta <elsile69@yahoo.com>
2020-01-06 08:37:08 +0200

    tig: latest commit

--- overlays/tig/default.nix
+++ overlays/tig/default.nix
@@ -8,15 +8,17 @@ with super; {
       paths = [
         (gitAndTools.tig.overrideAttrs (old: rec {
           pname = "tig";
-          version = "2.4.2";
+          version = "2.5.0";
           name = "${pname}-${version}";
 
           src = fetchFromGitHub {
             owner = "jonas";
             repo = pname;
-            rev = "6642c11425a2d7432d968146025b6d9a96db5d64";
-            sha256 = "0pm89f32cxmdqkd9r26z6fmsz41wpgkmgbk7aasq3pwj5gmrrpvc";
+            rev = "ca0809dbd20a31f6fa9b84c7eed5ecaa1368cd9d";
+            sha256 = "095c1gipw62nd4d6cgln2vdgyacf1zgmrr7h78sanmbnqqr2iwx8";
           };
+          patches = [];
+
         }))
       ];
 
