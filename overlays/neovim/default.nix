97c487ae0876aa4ae13aa77bcfe8871839afd7b3
vasile luta <elsile69@yahoo.com>
2020-01-21 00:21:06 +0200

    neovim: without config

--- overlays/neovim/default.nix
+++ overlays/neovim/default.nix
@@ -1,128 +1,46 @@
 self: super:
 with super;
+with lib;
 let
-  customPlugins = import ./plugins.nix { inherit self super; };
-  allPlugins = vimPlugins // customPlugins;
   lsp = import ../lsp self super;
-
-  loadPlugin = p: ''
-    set rtp^=${p.rtp}
-    set rtp+=${p.rtp}/after
-  '';
-
-  ftplugins = ''
-    filetype off | syntax off
-    ${super.lib.concatStrings (map loadPlugin ftPackages)}
-    filetype indent plugin on | syntax on
-  '';
-
-  ftPackages = with super.vimPlugins; [
-    vim-coffee-script
-    vim-jinja
-    vim-markdown
-    vim-json
-    vim-nix
-    customPlugins.vim-js
-    customPlugins.jsx
-    customPlugins.yats
-    customPlugins.twig
-  ];
-
-  init = builtins.readFile ./config/init.vim;
-  plugins = builtins.readFile ./config/plugins.vim;
-  options = builtins.readFile ./config/options.vim;
-  mappings = builtins.readFile ./config/mappings.vim;
-  commands = builtins.readFile ./config/commands.vim;
-  autocmds = builtins.readFile ./config/autocmds.vim;
-
-  neovimLuaEnv = lua.withPackages(ps:
-    (with ps; [ compat53 lpeg luabitop luv luv-dev mpack ]
-    ++ optionals doCheck [
-        nvim-client coxpcall busted luafilesystem penlight inspect
-      ]
-    ));
-
 in
 {
-
-  neovim-unwrapped = (neovim-unwrapped).overrideAttrs (old: rec {
-    name = "neovim-unwrapped-${version}";
-    version = "nightly";
-    src = fetchFromGitHub {
-      owner = "neovim";
-      repo = "neovim";
-      rev = "34a59242a0d42687a49119cca590e7b4203496ef";
-      sha256 = "0gs0h16jwr52k8mjb38bk69f4zkwzybm8l1vi5gadbqk7b3jvhjy";
-    };
-    NIX_CFLAGS_COMPILE = "-O3 -march=native";
-    buildInputs = old.buildInputs ++ [ utf8proc];
-  });
+  neovim-unwrapped = (neovim-unwrapped).overrideAttrs (
+    old: rec {
+      name = "neovim-unwrapped-${version}";
+      version = "nightly";
+      src = fetchFromGitHub {
+        owner = "neovim";
+        repo = "neovim";
+        rev = "5355cee77d7b3b62917036281406726832b6d7dc";
+        sha256 = "0qk8r84v6aa6f0maqd02c2xzcb6bj9ahx0mwf72qvc7gwc64843r";
+      };
+      NIX_CFLAGS_COMPILE = "-O3 -march=native";
+      buildInputs = old.buildInputs ++ [ utf8proc ];
+      nativeBuildInputs = old.nativeBuildInputs ++ [ makeWrapper ];
+      postInstall = old.postInstall + ''
+        wrapProgram $out/bin/nvim --prefix PATH : ${ makeBinPath
+        [
+          ag
+          lsp.js.vim-language-server
+          lsp.js.import-js
+          lsp.js.diagnostic-languageserver
+          nodePackages.typescript
+          nodePackages.eslint
+          nodePackages.eslint_d
+          nodePackages.prettier
+          nodePackages.typescript-language-server
+          nodePackages.vscode-html-languageserver-bin
+          nodePackages.vscode-css-languageserver-bin
+          nixpkgs-fmt
+        ]
+      }
+      '';
+    }
+  );
 
   neovim = neovim.override {
     withNodeJs = true;
-    extraMakeWrapperArgs = "--prefix PATH : ${stdenv.lib.makeBinPath
-      [
-        ag
-        fd
-        fzy
-        lsp.js.vim-language-server
-        lsp.js.import-js
-        nodePackages.typescript
-        nodePackages.eslint
-        nodePackages.typescript-language-server
-        nodePackages.vscode-html-languageserver-bin
-        nodePackages.vscode-css-languageserver-bin
-        nixpkgs-fmt
-      ]
-    }";
-
-    configure = {
-      packages.myVimPackage = with allPlugins; {
-
-        start = ftPackages ++ [
-          allfunc
-          async.vim
-          neomake
-          repeat
-          # nvim-lsp
-        ];
-
-        opt = [
-          vim-lsp
-          vim-picker
-          deoplete-nvim
-          ags
-          surround
-          tcomment_vim
-          vim-html-template-literals
-          vim-indent-object
-          quickfix-reflector-vim
-          vim-easy-align
-          multiple-cursors
-          vim-fugitive
-          vim-gitgutter
-          targets
-          vim-mergetool
-          conflict-marker
-          auto-git-diff
-          vim-dispatch
-          vim-edgemotion
-          vim-editorconfig
-          cool
-          retro
-          candy
-        ];
-      };
-
-      customRC = ''
-        ${ftplugins}
-        ${init}
-        ${plugins}
-        ${options}
-        ${mappings}
-        ${commands}
-        ${autocmds}
-      '';
-    };
+    configure = {};
   };
 }
