a59d9a51146220a80c1decaddec696db0ea51384
vasile luta <elsile69@yahoo.com>
2020-02-14 23:43:33 +0200

    popcorn: drop

--- overlays/popcorn/default.nix
+++
@@ -1,5 +0,0 @@
-self: super:
-let
-  url = https://github.com/timjrd/popcorntime.nix/archive/master.tar.gz;
-  popcornTime = ((import (builtins.fetchTarball url)) { });
-in { popcornTime = popcornTime; }
