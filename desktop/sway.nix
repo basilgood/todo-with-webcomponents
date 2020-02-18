3a440ac204499ce26a8db4efb1e6a2ad942f6210
vasile luta <elsile69@yahoo.com>
2020-02-18 07:40:27 +0200

    sway: fix and update module

--- desktop/sway.nix
+++ desktop/sway.nix
@@ -1,48 +1,70 @@
 { config, lib, pkgs, ... }:
+with lib;
 let
-  waybarConfig =
-    pkgs.writeText "config" (pkgs.callPackage ./waybar-config.nix {});
-  waybarStyle = pkgs.writeText "config" (builtins.readFile ./waybar.css);
+  cfg = config.programs.sway;
 in
 {
-  programs.sway = {
-    enable = true;
-    extraSessionCommands = ''
-      export XKB_DEFAULT_LAYOUT=us
-      export XKB_DEFAULT_OPTIONS=grp:alt_shift_toggle,caps:escape
-      export XDG_DATA_DIRS=${
-    let
-      schema = pkgs.gsettings-desktop-schemas;
-    in "${schema}/share/gsettings-schemas/${schema.name}"
-    }:$XDG_DATA_DIRS
-    '';
-    extraPackages = with pkgs; [
-      glib
-      jmtpfs
-      xwayland
-      xdg_utils
-      wl-clipboard
-      dmenu-wayland
-      nordic
-      paper-icon-theme
-      playerctl
-    ];
+
+  options.programs.sway = {
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+    };
+
+    terminal = mkOption {
+      type = types.str;
+      default = "${pkgs.alacritty}/bin/alacritty";
+    };
+
+    menu = mkOption {
+      type = types.str;
+      default = "${pkgs.dmenu-wayland}/bin/dmenu-wl_run";
+    };
+
+    status = mkOption {
+      type = types.str;
+      default = "${pkgs.waybar.override { pulseSupport = true; }}/bin/waybar";
+    };
   };
 
-  environment.etc."sway/config".text = with pkgs; ''
-    set $term ${alacritty}/bin/alacritty
-    set $menu ${dmenu-wayland}/bin/dmenu-wl_run
-    set $brightness ${brightnessctl}/bin/brightnessctl
-    set $grim ${grim}/bin/grim
-    set $mogrify ${imagemagick}/bin/mogrify
-    set $slurp ${slurp}/bin/slurp
-    set $mako ${mako}/bin/mako
-    set $idle ${swayidle}/bin/swayidle
-    set $swaylock ${swaylock}/bin/swaylock
-    set $lock $grim /tmp/lock.png && $mogrify -scale 10% -scale 1000% /tmp/lock.png && $swaylock -f -i /tmp/lock.png
-    set $status ${waybar.override { pulseSupport = true; }}/bin/waybar
-    output * bg ${./catalina.jpg} fill
-    ${builtins.readFile ./config}
-  '';
+  config = mkIf cfg.enable {
 
+    programs.sway = {
+      extraPackages = (with pkgs; [
+        glib
+        paper-icon-theme
+        nordic
+        xwayland
+        jmtpfs
+        xdg_utils
+        wl-clipboard
+        dmenu-wayland
+      ]
+      );
+      extraSessionCommands = ''
+        export XKB_DEFAULT_LAYOUT=us
+        export XDG_DATA_DIRS=${
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+      in "${schema}/share/gsettings-schemas/${schema.name}"
+      }:$XDG_DATA_DIRS
+      '';
+    };
+
+    environment.etc."sway/config".text = with pkgs; ''
+      set $swaylock ${swaylock}/bin/swaylock
+      set $term ${cfg.terminal}
+      set $brightness ${brightnessctl}/bin/brightnessctl
+      set $grim ${grim}/bin/grim
+      set $mogrify ${imagemagick}/bin/mogrify
+      set $slurp ${slurp}/bin/slurp
+      set $mako ${mako}/bin/mako
+      set $idle ${swayidle}/bin/swayidle
+      set $lock $grim /tmp/lock.png && $mogrify -scale 10% -scale 1000% /tmp/lock.png && $swaylock -f -i /tmp/lock.png
+      set $menu ${cfg.menu}
+      set $status ${cfg.status}
+      ${builtins.readFile ./config}
+      ${cfg.extraConfig}
+    '';
+  };
 }
