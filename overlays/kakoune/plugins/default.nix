ff425b59018109f8843d54d3e9ba0b3460bd9fa1
vasile luta <elsile69@yahoo.com>
2020-10-01 10:26:11 +0300

    brake: reuse repo

--- overlays/kakoune/plugins/default.nix
+++
@@ -1,5 +0,0 @@
-{ callPackage }: {
-  typescript = callPackage ./typescript.nix { };
-  find = callPackage ./find.nix { };
-  filetree = callPackage ./filetree.nix { };
-}
--- overlays/nixfmt/default.nix
+++
@@ -1,4 +0,0 @@
-self: super:
-{
-  nixpkgs-fmt = (import (fetchTarball https://github.com/nix-community/nixpkgs-fmt/archive/master.tar.gz) {});
-}
